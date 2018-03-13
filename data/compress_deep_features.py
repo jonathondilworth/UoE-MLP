73fa4b0a324fccf90c7e0967136b74e2ecd30022
sebastian <anse23@hotmail.com>
2018-03-13 09:02:22 +0000

    feat: Modified compression script for 2048 features and compress them

--- data/compress_deep_features.py
+++ data/compress_deep_features.py
@@ -7,16 +7,22 @@ def load_dataset(file_train, file_test):
 
     xtr, ytr = data_train[:,1:], data_train[:,0].astype(int)
     xte, yte = data_test[:,1:], data_test[:,0].astype(int)
-    
+
+    # xtr = xtr * 100
+    # xte = xte * 100
+
+    # xtr = xtr.astype(int)
+    # xte = xte.astype(int)
+
     return xtr, ytr, xte, yte
 
 def save(x, y, name):
     file = open(name, "wb")
-    np.savez(file, inputs=x, targets=y)
+    np.savez_compressed(file, inputs=x, targets=y)
 
 def compress(dataset_name):
-    path_to_test = "./{:s}_512_test.txt".format(dataset_name)
-    path_to_train = "./{:s}_512_train.txt".format(dataset_name)
+    path_to_test = "./{:s}_2048_test.txt".format(dataset_name)
+    path_to_train = "./{:s}_2048_train.txt".format(dataset_name)
 
     xtr, ytr, xte, yte = load_dataset(path_to_train, path_to_test)
     print(xtr.shape)
@@ -24,8 +30,8 @@ def compress(dataset_name):
     print(xte.shape)
     print(yte.shape)
 
-    path_to_npz_test = "./{:s}_512_test.npz".format(dataset_name)
-    path_to_npz_train = "./{:s}_512_train.npz".format(dataset_name)
+    path_to_npz_test = "./{:s}_2048_test.npz".format(dataset_name)
+    path_to_npz_train = "./{:s}_2048_train.npz".format(dataset_name)
     
     save(xtr, ytr, path_to_npz_train)
     save(xte, yte, path_to_npz_test)
